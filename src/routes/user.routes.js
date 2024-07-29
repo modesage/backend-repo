661d59ec10d848ee26ab140a7e2a2c731fe43f4d
Shaurya Dobhal <shauryadobhal76@gmail.com>
2024-07-29 11:00:05 +0530

    add login and logout phase

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,5 +1,6 @@
 import { Router } from "express";
-import { registerUser } from "../controllers/user.controller.js";
+import { registerUser, loginUser, logoutUser } from "../controllers/user.controller.js";
+import { verifyJWT } from "../middlewares/auth.middleware.js";
 import { upload } from "../middlewares/multer.middleware.js"
 
 const router = Router()
@@ -16,6 +17,10 @@ router.route("/register").post(
         }
     ]),
     registerUser
-    )
+)
+
+router.route("/login").post(loginUser)
+
+router.route("/logout").post(verifyJWT,  logoutUser)
 
 export default router
